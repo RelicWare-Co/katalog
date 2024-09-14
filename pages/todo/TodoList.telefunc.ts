1e69c419ad09ae4768ab34e3e242b4de3d515f69
Bati <no-reply@batijs.dev>
2024-09-14 10:03:37 -0500

    scaffold Vike app with Bati

---
+++ pages/todo/TodoList.telefunc.ts
@@ -0,0 +1,7 @@
+import * as drizzleQueries from "../../database/drizzle/queries/todos";
+import { getContext } from "telefunc";
+
+export async function onNewTodo({ text }: { text: string }) {
+  const context = getContext();
+  await drizzleQueries.insertTodo(context.db, text);
+}
